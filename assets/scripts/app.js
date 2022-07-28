c94f40aecf5bb580853f40403b8a31ea462f24f7
weby <danielparedesmilian@gmail.com>
2022-07-28 14:01:10 -0800

    initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,65 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function getUserInput() {
+	return parseInt(userInput.value);
+}
+
+function createAndWriteLog(operator, resultBeforeCalc, Calcnumber) {
+	const calcDesc = `${resultBeforeCalc} ${operator} ${Calcnumber}`;
+	outputResult(currentResult, calcDesc);
+}
+
+function writeToLog(
+	operationIdentifier,
+	prevResult,
+	operationNumber,
+	newResult
+) {
+	const logEntry = {
+		operation: operationIdentifier,
+		prevResult: prevResult,
+		number: operationNumber,
+		result: newResult,
+	};
+	logEntries.push(logEntry);
+	console.log(logEntries);
+}
+
+function add() {
+	const enteredNum = getUserInput();
+	const initialResult = currentResult;
+	currentResult += enteredNum;
+	createAndWriteLog("+", initialResult, enteredNum);
+	writeToLog("ADD", initialResult, enteredNum, currentResult);
+}
+
+function subtract() {
+	const enteredNum = getUserInput();
+	const initialResult = currentResult;
+	currentResult -= enteredNum;
+	createAndWriteLog("-", initialResult, enteredNum);
+	writeToLog("SUBTRACT", initialResult, enteredNum, currentResult);
+}
+function multiply() {
+	const enteredNum = getUserInput();
+	const initialResult = currentResult;
+	currentResult *= enteredNum;
+	createAndWriteLog("*", initialResult, enteredNum);
+	writeToLog("MULTIPLY", initialResult, enteredNum, currentResult);
+	t;
+}
+
+function divide() {
+	const enteredNum = getUserInput();
+	const initialResult = currentResult;
+	currentResult /= enteredNum;
+	createAndWriteLog("/", initialResult, enteredNum);
+	writeToLog("DIVIDE", initialResult, enteredNum, currentResult);
+}
+
+subtractBtn.addEventListener("click", subtract);
+addBtn.addEventListener("click", add);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
